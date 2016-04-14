e72517343893ae68293e8ed87747a2e8ad49e9c1
James Leddy <jwleddy@gmail.com>
2016-04-13 20:50:37 -0700

    added tic tac toe

---
+++ tictactoe/main.js
@@ -0,0 +1,20 @@
+function isSolved(board) {
+   if(/XXX......|...XXX...|......XXX|X..X..X|X...X...X|..X.X.X../.test(board.join(''))) return 'XXX';
+   if(/OOO......|...OOO...|......OOO|O..O..O|O...O...O|..O.O.O../.test(board.join(''))) return 'OOO';
+   if(board.every((x) => x === 'X' || x === 'O')) return 'BORING';
+}
+  for (i = 0; i < 9; i++) {
+    document.getElementsByClassName('square')[i].addEventListener('click',function() {
+    if (!this.textContent) {
+      var board = [];
+      this.textContent = document.getElementById('turn').textContent === 'X' ? 'X' : 'O';
+      document.getElementById('turn').textContent = document.getElementById('turn').textContent === 'X' ? 'O' : 'X';
+      for (i = 0; i < 9; i++) board.push(document.getElementsByClassName('square')[i].textContent || ' ');
+      if (isSolved(board)) {
+      	document.getElementById('turn').textContent = isSolved(board);
+        setTimeout(function(){ document.getElementById('turn').textContent = 'X'}, 1000);
+      	for (i = 0; i < 9; i++) document.getElementsByClassName('square')[i].textContent = '';
+      }
+    }
+  });
+}
